8a38c4cf3b63e0a7b51c67bc3f2dc08ccd8671b4
vasilii <porokh.vs@gmail.com>
2022-06-17 02:48:16 +0300

    process

--- src/modelViewer/studio.js
+++ src/modelViewer/studio.js
@@ -1,6 +1,12 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass.js';
+import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader.js';
+
 
 const BACK_COLOR = 0xDBE5FF
 
@@ -13,6 +19,10 @@ export const createStudio = (cubeMap) => {
     container.style.height = window.innerHeight + 'px';
 
     const scene = new THREE.Scene();
+    let composer, effectFXAA, outlinePass;
+
+    const forIntercepts = []
+
 
     const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, .01, 100);
     camera.position.set( 0, 2, -6);
@@ -23,6 +33,32 @@ export const createStudio = (cubeMap) => {
     renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(window.innerWidth, window.innerHeight)
     renderer.setClearColor(BACK_COLOR, 1)
+
+
+    			// postprocessing
+
+				composer = new EffectComposer( renderer );
+
+				const renderPass = new RenderPass( scene, camera );
+				composer.addPass( renderPass );
+
+				outlinePass = new OutlinePass( new THREE.Vector2( window.innerWidth, window.innerHeight ), scene, camera );
+				composer.addPass( outlinePass );
+
+				// const textureLoader = new THREE.TextureLoader();
+				// textureLoader.load( 'textures/tri_pattern.jpg', function ( texture ) {
+
+				// 	outlinePass.patternTexture = texture;
+				// 	texture.wrapS = THREE.RepeatWrapping;
+				// 	texture.wrapT = THREE.RepeatWrapping;
+
+				// } );
+                outlinePass.selectedObjects = forIntercepts
+
+
+				effectFXAA = new ShaderPass( FXAAShader );
+				effectFXAA.uniforms[ 'resolution' ].value.set( 1 / window.innerWidth, 1 / window.innerHeight );
+				composer.addPass( effectFXAA );
 
 
     container.appendChild( renderer.domElement );
@@ -64,7 +100,11 @@ export const createStudio = (cubeMap) => {
 
 
     let currentObject = null
-    const forIntercepts = []
+
+    document.addEventListener('click', () => {
+        //name.style.display = 'none'
+    })
+
 
     function onPointerClick( event ) {
         // calculate pointer position in normalized device coordinates
@@ -83,7 +123,11 @@ export const createStudio = (cubeMap) => {
             }
             intersects[0].object.material.color.set(0xff0000)
             currentObject = intersects[0].object
-            name.innerText = currentObject.name
+            title.classList.add('hidden')
+            setTimeout(() => { 
+                title.classList.remove('hidden')
+                title.innerText = currentObject.name
+                name.style.display = 'block'}, 110) 
         }
     }
 
@@ -95,9 +139,17 @@ export const createStudio = (cubeMap) => {
         raycaster.setFromCamera( pointer, camera );
         // calculate objects intersecting the picking ray
         const intersects = raycaster.intersectObjects(forIntercepts);
+        for (let i = 0; i < forIntercepts.length; ++i) {
+            if (currentObject === forIntercepts[i]) {
+                continue;
+            }
+            forIntercepts[i].material.color.set(0xFFF6DB)
+        }
         if (intersects[0]) {
-            renderer.domElement.style.cursor = 'pointer'
-
+            if (currentObject !== intersects[0].object) {
+                renderer.domElement.style.cursor = 'pointer'
+                intersects[0].object.material.color.set(0xff9999)
+            }
         } else {
             renderer.domElement.style.cursor = 'auto'
         }
@@ -113,6 +165,23 @@ export const createStudio = (cubeMap) => {
     name.classList.add('objName')
     document.body.appendChild(name)
 
+    const title = document.createElement('div')
+    title.classList.add('title')
+    name.appendChild(title)
+
+    const closeName = document.createElement('div')
+    closeName.classList.add('closeName')
+    name.appendChild(closeName)
+    closeName.innerText = 'close'
+    closeName.addEventListener('click', () => {
+        currentObject.material.color.set(0xFFF6DB)
+        currentObject = null
+        name.style.display = 'none'
+    })
+
+
+
+
 
     return {
         addToScene(model) {
@@ -125,7 +194,8 @@ export const createStudio = (cubeMap) => {
             if (!camera) {
                 return
             }
-            renderer.render( scene, camera );
+            composer.render();
+            //renderer.render( scene, camera );
         },
         resize () {
             if (!camera) {
